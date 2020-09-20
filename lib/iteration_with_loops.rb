fd53f1516885b8f3f32b899baeb3e98431f025a7
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-20 22:53:37 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,14 @@
 require 'pry'
 def join_nested_strings(src)
-  final_str = " "
+  final_str = ""
   row_index = 0 
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do
-      final_str += src[row_index][element_index]
+      final_str += src[row_index][element_index].to_s
       element_index += 1 
     end
     row_index += 1
   end
+  final_str
 end
-final_str
